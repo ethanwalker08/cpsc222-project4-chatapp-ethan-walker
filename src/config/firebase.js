1112244c09726419ddc8eeb2043c2617d42d5965
Ethan Walker <ethanwalker23081@gmail.com>
2022-10-24 11:20:07 -0400

    update firebase.js config file
    the config file will need to be configured by users now instead of directly linking to my firebase project database.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDAsKkBPQTGk-eyiaqMBkNh3XpLBkI9nw4",
-  authDomain: "cpsc222-project4.firebaseapp.com",
-  projectId: "cpsc222-project4",
-  storageBucket: "cpsc222-project4.appspot.com",
-  messagingSenderId: "282983387502",
-  appId: "1:282983387502:web:2df273d5c74f021c25317f"
+  apiKey: "#{APIKEY}",
+  authDomain: "#{DOMAIN}",
+  projectId: "#{PROJECT_ID}",
+  storageBucket: "#{BUCKET}",
+  messagingSenderId: "#{SENDER_ID}",
+  appId: "#{APP_ID}"
 };
 
 firebase.initializeApp(firebaseConfig);
